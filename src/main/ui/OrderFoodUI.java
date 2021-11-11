0c5838390df1dfabea14fb55a3bb8a7498e2863c
rebeccaliu <kej19@student.ubc.ca>
2021-11-11 01:45:02 -0800

    Finish OrderFoodUI window design without event handling

--- src/main/ui/OrderFoodUI.java
+++ src/main/ui/OrderFoodUI.java
@@ -14,18 +14,17 @@ import java.io.IOException;
 public class OrderFoodUI extends JFrame implements ActionListener {
     private static final String JSON_STORE = "./data/foodToOrderList.json";
     private FoodToOrderList ftoList;
-    private static final int POSITION = 90;
+    private static final int XPOSITION = 540;
     private static final int WIDTH = 250;
-    private static final int HEIGHT = 30;
+    private static final int HEIGHT = 50;
     private JButton orderButton;
     private JButton quitButton;
     private JButton saveButton;
     private JButton loadButton;
     private FoodListUI foodListUI;
-    private ImageIcon imageIcon;
+    private ImageIcon backgroundImageIcon;
     private JLabel selectionLabel;
     private JLabel backgroundLabel;
-    private JsonReader jsonReader;
 
 
     // This method references code from this website
@@ -34,37 +33,66 @@ public class OrderFoodUI extends JFrame implements ActionListener {
     public OrderFoodUI() {
         super("Order Food Application");
         setDefaultCloseOperation(EXIT_ON_CLOSE);
-        setPreferredSize(new Dimension(450, 500));
+        setPreferredSize(new Dimension(847, 960));
         ((JPanel) getContentPane()).setBorder(new EmptyBorder(13, 13, 13, 13));
         setLayout(null);
 
         init();
 
+        selectionLabel.setBounds(470, 20, 410, 100);
+        add(selectionLabel);
+        selectionLabel.setForeground(Color.DARK_GRAY);
+        selectionLabel.setFont(new Font("Arial", Font.BOLD, 20));
+
+        addButtons();
+
+        setBackgroundImage();
+
+        pack();
+        setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        setLocationRelativeTo(null);
+        setVisible(true);
+        setResizable(false);
     }
 
     //MODIFIES: this
-    //EFFECTS: initializes foodToOrderList, buttons, labels, and image icon
+    //EFFECTS: initializes foodToOrderList, buttons, labels, button panel and image icon
     private void init() {
         ftoList = new FoodToOrderList();
-        orderButton = new JButton("Order your food");
-        quitButton = new JButton("Quit the APP");
+        orderButton = new JButton("Start your order");
         saveButton = new JButton("Save your order");
         loadButton = new JButton("Load your saved order");
-        selectionLabel = new JLabel("Please select from the following choices", JLabel.CENTER);
-        imageIcon = new ImageIcon("./images/background1.jpeg");
-        backgroundLabel = new JLabel("", imageIcon, JLabel.CENTER);
+        quitButton = new JButton("Quit the APP");
+        selectionLabel = new JLabel("Please select from: ", JLabel.CENTER);
+        backgroundImageIcon = new ImageIcon("./images/background1.jpeg");
+        backgroundLabel = new JLabel("", backgroundImageIcon, JLabel.CENTER);
+
     }
 
 
-    //MODIFIES: this
-    //EFFECTS: loads food-to-order list from file
-    private void loadFoodToOrderList() {
-        try {
-            ftoList = jsonReader.read();
-            System.out.println("Loaded the food-to-order list from " + JSON_STORE);
-        } catch (IOException e) {
-            System.out.println("Unable to read from file: " + JSON_STORE);
-        }
+    //EFFECTS: add buttons on the main window
+    private void addButtons() {
+        setButton(orderButton, 100);
+        setButton(saveButton, 160);
+        setButton(loadButton, 220);
+        setButton(quitButton, 280);
+
+    }
+
+    //EFFECTS: sets a button at given y-position
+    private void setButton(JButton button, int yposition) {
+        button.setBounds(XPOSITION, yposition, WIDTH, HEIGHT);
+        button.setFont(new Font("Arial", Font.PLAIN, 17));
+        add(button);
+        button.setActionCommand(button.getName());
+        button.addActionListener(this);
+        button.setForeground(Color.BLACK);
+    }
+
+    //EFFECTS: set the background image of the main window
+    private void setBackgroundImage() {
+        backgroundLabel.setBounds(0, 0, 847, 960);
+        add(backgroundLabel);
     }
 
 
